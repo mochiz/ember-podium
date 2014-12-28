3dfc44d7b0b2ef4cd72b1c97e217f997ef0807da
mochiz <mochi.hiro43@gmail.com>
2014-12-29 04:15:02 +0900

    サンプルデータを作成した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Result.destroy_all
+Race.destroy_all
+Rider.destroy_all
+races = YAML.load_file(Rails.root.join('db', 'fixtures', 'races.yml'))
+races.each do |key, value|
+  Race.create(name: value['name'],
+              icon: value['icon'],
+              image: value['image'],
+              start_at: value['start_at'],
+              end_at: value['end_at'],
+              url: value['url'])
+end
+
+riders = YAML.load_file(Rails.root.join('db', 'fixtures', 'riders.yml'))
+riders.each do |key, value|
+  Rider.create(name: value['name'],
+               avatar: value['avatar'],
+               team: value['team'])
+end
+
+results = YAML.load_file(Rails.root.join('db', 'fixtures', 'results.yml'))
+results.each do |key, value|
+  Result.create(race: Race.find_by(name: value['race']),
+                rider: Rider.find_by(name: value['rider']),
+                position: value['position'])
+end
